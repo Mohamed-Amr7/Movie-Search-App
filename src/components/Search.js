a38f33c7f0e1567b74f591198f3a7a28b555388f
Mohamed-Amr7 <moh.amr777@gmail.com>
2023-12-20 16:16:47 +0200

    Implemented Search component

---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Search = (props) => {
+    const [inputValue, setInputValue] = useState("");
+
+    const resetInputField = () => {
+        setInputValue("");
+    };
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const performSearch = (e) => {
+        e.preventDefault();
+        props.search(inputValue);
+        resetInputField();
+    };
+
+    return (
+        <form className="search">
+            <input
+                value={inputValue}
+                onChange={handleInputChange}
+                type="text"
+            />
+            <input onClick={performSearch} type="submit" value="SEARCH" />
+        </form>
+    );
+};
+
+export default Search;
